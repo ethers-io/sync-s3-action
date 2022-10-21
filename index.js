618113c0d6203553ddb00fc2d5776a47d7cc87f3
Richard Moore <github@ricmoo.com>
2022-10-20 20:40:48 -0400

    Pass correct bucket key in options.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { syncS3 } from "./sync-s3.js";
 syncS3({
     accessKeyId: core.getInput("aws_access_key_id", { required: true }),
     secretAccessKey: core.getInput("aws_secret_access_key", { required: true }),
-    s3Bucket: core.getInput("aws_s3_bucket", { required: true }),
+    bucket: core.getInput("aws_s3_bucket", { required: true }),
     sourceFolder: core.getInput("source_folder", { required: true }),
     sourcePrefix: core.getInput("source_prefix", { required: false }),
     destinationPrefix: core.getInput("destination_prefix", { required: false }),
